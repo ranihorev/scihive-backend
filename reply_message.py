27b029021172570249686c65a4f94897d3f19d59
David Allen <dawork20100@gmail.com>
2019-07-24 11:35:02 -0700

    Functions now more versatile

--- reply_message.py
+++ reply_message.py
@@ -1,17 +1,31 @@
 import requests
 
-def send_reply_message():
+
+def send_message(to, subject, text, html):
     requests.post(
         "https://api.mailgun.net/v3/email.scihive.org/messages",
         auth=("api", "API KEY"),
         data={"from": "SciHive <mailgun@email.scihive.org>",
-              "to":["USER@EMAIL.com"],
+              "to":[to],
+              "subject": subject,
+              "text":text,
+              "html":html
+             }
+    )
+
+def send_reply_message(to, user_1, user_2, paper_name, paper_id, comment, reply):
+    requests.post(
+        "https://api.mailgun.net/v3/email.scihive.org/messages",
+        auth=("api", "API KEY"),
+        data={"from": "SciHive <mailgun@email.scihive.org>",
+              "to":[to],
               "subject": "Reply to your comment on SciHive",
-              "text":"""Hello, User!\nJohn Doe has replied to your comment on Paper X:
-                     \n{COMMENT}\n~User\n{REPLY}\n~John Doe\n\nClick here to view site.""",
-              "html":"""<p><b>Hello, User!</b><br>John Doe has replied to your comment on Paper X:</p>
-                     <p>{COMMENT}<br>~User</p><p>{REPLY}<br>~John Doe</p><br><br>
-                     <a href='https://www.scihive.org'>Click here to view site.</a>"""
+              "text":"""Hello, {0}!\n{1} has replied to your comment on {2}:
+                     \n{3}\n~{0}\n{4}\n~{1}\nClick here to view site.""".format(user_1, user_2, paper_name, comment, reply),
+              "html":"""<p><b>Hello, {0}!</b><br>{1} has replied to your comment on <u>{2}</u>:</p>
+                     <p>{3}<br>~{0}</p><p>{4}<br>~{1}</p><br>
+                     <a href='https://www.scihive.org/paper/{5}'>Click here to view site.</a>""".format(user_1, user_2, paper_name, 
+                                                                                                        comment, reply, paper_id)
              }
     )
 
