b6fe0e340794690cf9c2ba622159f9691644d24a
Rani <ranihorev@gmail.com>
2020-05-15 23:12:00 -0700

    background tasks

--- src/run_background_tasks.py
+++ src/run_background_tasks.py
@@ -1,6 +1,6 @@
 import logging
 import threading
-
+import os
 import schedule
 import time
 from src.new_backend.scrapers.arxiv import run as run_arxiv
@@ -17,9 +17,9 @@ def run_scheduled_tasks():
     logger = logging.getLogger(__name__)
 
     logger.info('Start background tasks')
-    schedule.every(30).minutes.do(run_threaded, main_twitter_fetcher)
-    schedule.every(6).hours.do(run_threaded, run_paperswithcode)
-    schedule.every(3).hours.do(run_threaded, run_arxiv)
+    schedule.every(int(os.environ.get('TWITTER_FREQ_HOURS', 3.15))).hours.do(run_threaded, main_twitter_fetcher)
+    schedule.every(int(os.environ.get('PAPERS_WITH_CODE_FREQ_HOURS', 6.2))).hours.do(run_threaded, run_paperswithcode)
+    schedule.every(int(os.environ.get('ARXIV_FREQ_HOURS', 4.2))).hours.do(run_threaded, run_arxiv)
 
     while True:
         schedule.run_pending()
