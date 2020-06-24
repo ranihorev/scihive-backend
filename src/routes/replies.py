e28bb513360c659f835b5931eae92d8c8b7f7d3a
s0h3ck <s0h3ck@gmail.com>
2020-06-23 20:21:16 -0400

    Add jwt_required

--- src/routes/replies.py
+++ src/routes/replies.py
@@ -2,12 +2,12 @@ import logging
 from datetime import datetime
 
 from flask import Blueprint
-from flask_jwt_extended import get_jwt_identity, jwt_optional, jwt_required
-from flask_restful import (Api, Resource, abort, fields, inputs, marshal_with,
-                           reqparse)
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_restful import (Api, Resource, abort, fields, inputs, marshal_with, reqparse)
 from sqlalchemy import or_
 from typing import Optional
 from src.new_backend.models import Collection, Comment, Paper, Reply, db, User
+from .user_utils import get_user
 
 app = Blueprint('replies', __name__)
 api = Api(app)
@@ -23,28 +23,31 @@ replies_fields = {
 EMPTY_FIELD_MSG = 'This field cannot be blank'
 
 class ReplyResource(Resource):
+    method_decorators = [jwt_required]
 
     def _get_reply(self, reply_id):
 
         reply = Reply.query.get_or_404(reply_id)
+
+        user = get_user()
+
+        if reply.user != user:
+            abort(403, message='unauthorized to modify the reply')
 
         return reply
 
 
     @marshal_with(replies_fields, envelope='reply')
     def post(self, reply_id):
+
         reply = self._get_reply(reply_id)
 
         edit_reply_parser = reqparse.RequestParser()
-        print(edit_reply_parser)
         edit_reply_parser.add_argument('text', help=EMPTY_FIELD_MSG, type=str, location='json', required=False)
-        #edit_comment_parser.add_argument('visibility', help=EMPTY_FIELD_MSG, type=visibilityObj, location='json',
-        #                                 required=True)
         data = edit_reply_parser.parse_args()
-        print(data)
         reply.text = data['text']
-        db.session.commit()
 
+        db.session.commit()
 
         return reply
 
