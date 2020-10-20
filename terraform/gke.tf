3ee6dc0de72fc3b76750879907e07205a91cd03b
Rani <ranihorev@gmail.com>
2020-10-19 17:50:18 -0700

    dev and prod configs Added terraform

---
+++ terraform/gke.tf
@@ -0,0 +1,73 @@
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
+}
+
+variable "node_locations" {
+  description = "node zones"
+}
+
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  node_locations = var.node_locations
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+
+  master_auth {
+    username = var.gke_username
+    password = var.gke_password
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${google_container_cluster.primary.name}-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
+
+  node_locations = var.node_locations
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
+output "kubernetes_cluster_name" {
+  value       = google_container_cluster.primary.name
+  description = "GKE Cluster Name"
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+project_id = "scihive-prod"
+region     = "us-central1"
+gke_num_nodes = 2
+node_locations = [
+    "us-central1-c",
+  ]
