56b27010a90eb8641943d903287e531c5e313deb
Rani <ranihorev@gmail.com>
2020-10-26 21:51:56 -0700

    terraform fixes

--- terraform/db.tf
+++ terraform/db.tf
@@ -11,7 +11,7 @@ resource "google_sql_database_instance" "main_primary" {
   database_version = "POSTGRES_11"
   depends_on       = [google_service_networking_connection.private_vpc_connection]
   settings {
-    tier              = "custom-1-3840"
+    tier              = "db-custom-1-3840"
     availability_type = "ZONAL"
     disk_size         = 10 # 10 GB is the smallest disk size
     location_preference {
@@ -22,6 +22,10 @@ resource "google_sql_database_instance" "main_primary" {
     }
     backup_configuration {
       enabled = true
+    }
+    maintenance_window {
+      day  = 7
+      hour = 0
     }
   }
 }
